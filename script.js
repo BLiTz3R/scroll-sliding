494de240c8d9bf7e3849bd06d13ae64a505a000d
George <bl1723r@gmail.com>
2019-03-18 19:17:17 +0200

    init

---
+++ script.js
@@ -0,0 +1,34 @@
+// Debounce function to prevent scroll events flood, runs function once every 20ms by default
+function debounce(func, wait = 20, immediate = true) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+        timeout = null;
+        if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
+}
+
+const sliderImages = document.querySelectorAll('.slide-in');
+
+function checkSlide(e) {
+    sliderImages.forEach(sliderImage => {
+        const slideInAt = (window.scrollY + window.innerHeight) - (sliderImage.height / 2); // make it slide in when scrolled at half the image's height
+        const imageBottom = sliderImage.offsetTop + sliderImage.height; // bottom of the image
+        const isHalfShown = slideInAt > sliderImage.offsetTop;
+        const isNotScrolledPast = window.scrollY < imageBottom;
+        if (isHalfShown && isNotScrolledPast) {
+            sliderImage.classList.add('active');
+        } else {
+            sliderImage.classList.remove('active');
+        }
+    })
+}
+
+// Event listener, with checkSlide() ran through debounce 
+window.addEventListener('scroll', debounce(checkSlide));
